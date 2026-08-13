3bb614916f9126533bc62eb4bc1445687c2ad6aa
Ilya Shevchenko <id.shev@yandex.ru>
2026-08-13 20:07:06 +0200

    Update add/update schema

--- backend/src/modules/students/student-schemas.js
+++ backend/src/modules/students/student-schemas.js
@@ -1,35 +1,37 @@
 const { z } = require("zod");
 
+const sqlSafeString = () => z.string().stringFormat(/([A-Za-z0-9 ])+/).trim();
+
 const getAllStudentsSchema = z.object({
     page: z.coerce.number().int().positive().default(1).optional(),
     limit: z.coerce.number().int().positive().default(10).optional(),
-    search: z.string().trim().optional(),
-    class: z.string().trim().optional(),
-    section: z.string().trim().optional(),
+    search: sqlSafeString().optional(), // Limit to safe SQL characters and spaces
+    class: sqlSafeString().optional(),
+    section: sqlSafeString().optional(),
 });
 
 const addStudentSchema = z.object({
     userId: z.coerce.number().int().positive().optional(),
-    name: z.string().trim().min(1, "Name is required").optional(),
-    gender: z.string().trim().optional(),
-    phone: z.string().trim().optional(),
-    email: z.string().trim().email("Invalid email").optional(),
-    dob: z.string().trim().optional(),
-    currentAddress: z.string().trim().optional(),
-    permanentAddress: z.string().trim().optional(),
-    fatherName: z.string().trim().optional(),
-    fatherPhone: z.string().trim().optional(),
-    motherName: z.string().trim().optional(),
-    motherPhone: z.string().trim().optional(),
-    guardianName: z.string().trim().optional(),
-    guardianPhone: z.string().trim().optional(),
-    relationOfGuardian: z.string().trim().optional(),
+    name: sqlSafeString().min(1, "Name is required").optional(),
+    gender: sqlSafeString().optional(),
+    phone: z.e164().optional(), // phone number in E.164 format
+    email: z.email().optional(),
+    dob: sqlSafeString().optional(),
+    currentAddress: z.string().stringFormat(/([A-Za-z0-9 ,.])+/).trim().optional(),
+    permanentAddress: z.string().stringFormat(/([A-Za-z0-9 ,.])+/).trim().optional(),
+    fatherName: sqlSafeString().optional(),
+    fatherPhone: z.e164().optional(), // phone number in E.164 format
+    motherName: sqlSafeString().optional(),
+    motherPhone: z.e164().optional(), // phone number in E.164 format
+    guardianName: sqlSafeString().optional(),
+    guardianPhone: z.e164().optional(), // phone number in E.164 format
+    relationOfGuardian: sqlSafeString().optional(),
     systemAccess: z.coerce.boolean().optional(),
-    class: z.string().trim().optional(),
-    section: z.string().trim().optional(),
-    admissionDate: z.string().trim().optional(),
+    class: sqlSafeString().optional(),
+    section: sqlSafeString().optional(),
+    admissionDate: sqlSafeString().optional(),
     roll: z.coerce.number().int().positive("Roll must be a positive integer").optional(),
-}).passthrough();
+});
 
 module.exports = {
     getAllStudentsSchema,
